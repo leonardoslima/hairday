a4e7eba8768fa1c984cd2a3ca9aca53a4fcbba4a
Leonardo Silva <leonardosivalima93@live.com>
2024-10-29 15:16:59 -0300

    enviando um horário selecionado para a API

--- src/modules/form/subimit.js
+++ src/modules/form/subimit.js
@@ -1,6 +1,7 @@
 import dayjs from "dayjs";
 
 const form = document.querySelector("form");
+const clienteName = document.getElementById("client");
 const selectedDate = document.getElementById("date");
 
 // Date atual para formatar o input
@@ -13,4 +14,32 @@ selectedDate.min = inputToday;
 form.submit = (event) => {
   // Previne o comportamento padrão de carregar a página.
   event.preventDefault();
+
+  try {
+    // Recuperando o nome do cliente
+    const name = clienteName.value.trim();
+
+    if (!name) {
+      return alert("Informe o nome do cliente!");
+    }
+
+    // Recuperar o horário selecionado.
+    const hourSelected = document.querySelector(".hour-selected");
+
+    if (!hourSelected) {
+      return alert("Selecione a hora");
+    }
+
+    // Recuperar somente a hora
+    const [hour] = hourSelected.innerText.split(":");
+
+    // Insere a hora na data
+    const when = dayjs(selectedDate.value).add(hour, "hour");
+
+    // Gera um ID
+    const id = new Date().getTime();
+  } catch (error) {
+    alert("Não foi possível realizar o agendamento.");
+    console.log(error);
+  }
 };
